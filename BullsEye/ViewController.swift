a7be15cd44da796cbb9b33928c0d1568b10a3406
rasyidrw <rasyid@mymindstores.com>
2019-07-24 14:09:37 +0700

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Rasyid Respati Wiriaatmaja on 24/07/19.
+//  Copyright © 2019 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
